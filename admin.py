3b8a51ddf0b0c2ff91a427d987a9f85d57f8ad1c
Will <gnann@ime.usp.br>
2019-01-23 19:07:43 -0200

    Corrigindo o admin

--- admin.py
+++ admin.py
@@ -8,46 +8,14 @@ class ParecerDisciplinaInline(admin.TabularInline):
     model = ParecerDisciplina
     verbose_name = "disciplina"
 
-class DisciplinaAdmin(admin.ModelAdmin):
-    fields = ['sigla', 'nome']
-
-class RequerimentoAlteracaoAdmin(admin.ModelAdmin):
-    fieldsets = [
-        ("Informações do aluno",  {'fields':
-            [('aluno', 'unidade'), ('disciplina', 'turma')]
-        }),
-        ("Informações do parecer", {'fields':
-            ['docente', 'data_parecer', ('frequencia', 'nota'), 'observacao']
-        })
-    ]
-
 class RequerimentoMatriculaAdmin(admin.ModelAdmin):
-    fieldsets = [
-        ("Informações do aluno", {'fields': [('aluno', 'unidade')]}),
-        ("Informações do parecer", {'fields':
-            ['docente', 'data_parecer', 'observacao']
-        })
-    ]
     inlines = [ParecerDisciplinaInline]
-
-class RequerimentoOutrosAdmin(admin.ModelAdmin):
-    fieldsets = [
-        ("Informações do aluno", {'fields':
-            [('aluno', 'unidade'), 'solicitacao']
-        }),
-        ("Informações do parecer", {'fields':
-            ['docente', 'data_parecer', 'parecer', 'observacao']
-        })
-    ]
-
-class ProtocoloAvulsoAdmin(admin.ModelAdmin):
-    pass
 
 admin.site.register(Aluno)
 admin.site.register(Docente)
-admin.site.register(Disciplina, DisciplinaAdmin)
+admin.site.register(Disciplina)
 admin.site.register(Unidade)
-admin.site.register(RequerimentoAlteracao, RequerimentoAlteracaoAdmin)
+admin.site.register(RequerimentoAlteracao)
 admin.site.register(RequerimentoMatricula, RequerimentoMatriculaAdmin)
-admin.site.register(RequerimentoOutros, RequerimentoOutrosAdmin)
-admin.site.register(ProtocoloAvulso, ProtocoloAvulsoAdmin)
+admin.site.register(RequerimentoOutros)
+admin.site.register(ProtocoloAvulso)
